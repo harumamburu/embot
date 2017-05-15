7be53e9e561dc70368b7fc7bfbbb19f49ec82621
harumamburu <nikolay.kovalev92@gmail.com>
2017-05-15 19:05:02 +0300

    Fix visit form locators

--- src/main/java/com/mylab/embot/page/element/VisitorForm.java
+++ src/main/java/com/mylab/embot/page/element/VisitorForm.java
@@ -7,18 +7,21 @@ import ru.yandex.qatools.htmlelements.element.TextInput;
 
 public class VisitorForm extends HtmlElement {
 
-    @FindBy(xpath = "//input[contains(@id, 'visitCategory')]")
+    @FindBy(xpath = "//input[contains(@id, 'firstname')]")
     private TextInput nameInput;
 
-    @FindBy(xpath = "//input[contains(@id, 'embassy')]")
-    private TextInput surnameInput;
+    @FindBy(xpath = "//input[contains(@id, 'lastname')]")
+    private TextInput lastnameInput;
 
-    @FindBy(xpath = "//input[contains(@id, 'visitorsCount')]")
+    @FindBy(xpath = "//input[contains(@id, 'phone')]")
+    private TextInput phoneInput;
+
+    @FindBy(xpath = "//input[contains(@id, 'email')]")
     private TextInput emailInput;
 
-    @FindBy(xpath = "//input[contains(@id, 'visitDatetime')]")
+    @FindBy(xpath = "//input[contains(@id, 'document')]")
     private TextInput passId;
 
-    @FindBy(xpath = "//select[@name='undefined']")
+    @FindBy(xpath = "//select[contains(@id, 'representation')]")
     private Select representationSelect;
 }
