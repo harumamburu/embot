0b1086f695869726baf238dee4b74a796923f359
harumamburu <nikolay.kovalev92@gmail.com>
2017-05-16 16:07:32 +0300

    Extend calendar block capabilities
    We need to check if there are available time slots in the calendar and pick them up, if any

--- src/main/java/com/mylab/embot/page/element/Calendar.java
+++ src/main/java/com/mylab/embot/page/element/Calendar.java
@@ -1,21 +1,59 @@
 package com.mylab.embot.page.element;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.springframework.beans.factory.annotation.Value;
+import ru.yandex.qatools.htmlelements.element.Button;
 import ru.yandex.qatools.htmlelements.element.HtmlElement;
 
-public class Calendar extends HtmlElement {
+import java.util.Optional;
 
-    @Value("element.calendar.locator://div[@id='ui-datepicker-div']")
-    private String calendarLocator;
+public class Calendar extends HtmlElement {
 
     @Value("element.calendar.locator://td[not(contains(@class, 'unselectable'))]")
     private String openDayLocator;
 
-    public String getCalendarLocator() {
-        return calendarLocator;
+    @FindBy(className = "ui-datepicker-next")
+    private Button nextMonthButton;
+
+    private Button openDayButton;
+
+    public boolean chooseAvailableDay() {
+        if (isSlotAvailable()) {
+            openDayButton.click();
+            return true;
+        }
+        return false;
     }
 
-    public String getOpenDayLocator() {
-        return openDayLocator;
+    public String getAvailableDayText() {
+        if (isSlotAvailable()) {
+            return openDayButton.getWrappedElement().getText();
+        }
+        return null;
+    }
+
+    public boolean isSlotAvailable() {
+        if (openDayButton == null) {
+            Optional.ofNullable(
+                    Optional.ofNullable(findElement(By.xpath(openDayLocator)))
+                            .orElseGet(() -> {
+                                nextMonthButton.click();
+                                return findElement(By.xpath(openDayLocator));
+                            }))
+                    .ifPresent(webElement -> openDayButton = new Button(webElement));
+        }
+        return openDayButton == null;
+    }
+
+    @Override
+    public WebElement findElement(By by) {
+        try {
+            return super.findElement(by);
+        } catch (NoSuchElementException e) {
+            return null;
+        }
     }
 }
