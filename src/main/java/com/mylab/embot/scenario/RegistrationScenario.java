55a1fef30ab2ebdf4dae9964520f6b9200ba7dbc
harumamburu <nikolay.kovalev92@gmail.com>
2017-05-18 12:27:49 +0300

    Add skype client usage to scenario

--- src/main/java/com/mylab/embot/scenario/RegistrationScenario.java
+++ src/main/java/com/mylab/embot/scenario/RegistrationScenario.java
@@ -4,6 +4,7 @@ import com.mylab.embot.driver.CustomChromeDriver;
 import com.mylab.embot.entity.User;
 import com.mylab.embot.page.MainPage;
 import com.mylab.embot.page.RegisterPage;
+import com.mylab.embot.skype.SkypeClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
@@ -13,6 +14,12 @@ public class RegistrationScenario implements Scenario {
 
     @Autowired
     private CustomChromeDriver driver;
+
+    @Autowired
+    private SkypeClient skypeClient;
+
+    @Value("${skype.group.name:SlotsNotifications}")
+    private String slotsTopic;
 
     @Value("scenario.entrypoint.address")
     private String mainPageAddress;
@@ -31,6 +38,7 @@ public class RegistrationScenario implements Scenario {
 
         String slotsDay = page.getAvailableSlotDay(visitors.size());
         if(slotsDay != null) {
+            skypeClient.sendGroupNotification(slotsTopic, "Slots available for " + slotsDay + "!!");
             page.registerVisitors(visitors);
         } else {
 
